7b2a6f39fe9ec684976da599b773072412481f22
DanielRealpe <daniel_realpee@soy.sena.edu.co>
2024-04-05 09:41:13 -0500

    par o impar

--- dani/index.js
+++ dani/index.js
@@ -8,3 +8,13 @@ function mayor(a, b) {
 console.log(mayor(4, 6))
 console.log(mayor(100, 6))
 console.log(mayor(4, 101))
+
+const parImpar = (numero) => {
+    if (numero % 2 === 0) {
+        return "Es par"
+    } else {
+        return "Es impar"
+    }
+}
+
+console.log(parImpar(3));
